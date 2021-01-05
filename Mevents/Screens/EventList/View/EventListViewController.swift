6272f4f596358fbb3db502c0da5eacfa77f2f3a7
Isnard Silva <isnardsilva@icloud.com>
2021-01-05 10:01:09 -0300

    Implement coordinator pattern

--- Mevents/Screens/EventList/View/EventListViewController.swift
+++ Mevents/Screens/EventList/View/EventListViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 final class EventListViewController: UIViewController {
     // MARK: - Properties
+    weak var coordinator: MainCoordinator?
     
     
     // MARK: - View Life Cycle
+    override func loadView() {
+        super.loadView()
+        self.view.backgroundColor = .systemBackground
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.title = "Mevent"
     }
 }
 
